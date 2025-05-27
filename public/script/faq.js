5531f2f5ce37f1fe20d920fde2f0dd8b8ec77cfd
liam woodward <liamwoodward20@outlook.com>
2025-05-27 14:35:51 +0100

    making changes

---
+++ public/script/faq.js
@@ -0,0 +1,13 @@
+const questionToggle = document.querySelector('.question');
+
+
+
+
+questionToggle.addEventListener('click', function(){
+  const question = document.querySelector('.question');
+  
+ 
+  if (question.style.display === 'block') {
+     answer.style.display = 'none'
+  }
+})
