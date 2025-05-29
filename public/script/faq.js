4699fd293da12efb4de3f8ff53fb42965a9f41ff
liam woodward <liamwoodward20@outlook.com>
2025-05-29 15:13:00 +0100

    updating html

--- public/script/faq.js
+++ public/script/faq.js
@@ -1,20 +1,17 @@
-const questionToggle = document.querySelector('.question-panel');
+const questionToggle = document.querySelectorAll('.question-panel');
 
 
 
-  const answer = document.querySelector('.answer');
-  
-  
-  
+  const answer = document.getElementsByTagName('div');
 
   questionToggle.addEventListener('click', function(){
-    for (let i = 0; i < answer.length; i++) {
-      answer[i].style.display = 'none'
-    }
-   if ( answer.style.display === 'none') {
+   for (let i = 0; i < answer.length; i++) {
+    answer[i].style.display = 'none'
+   }
+})
+
+ if ( answer.style.display === 'none') {
     answer.style.display = 'block';
    } else {
     answer.style.display = 'none'
    }
-})
-
